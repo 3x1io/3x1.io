0d6f22eba636536f713ca6d12d359aad04cfeb23
Fady Mondy <info@3x1.io>
2024-08-14 14:23:45 +0300

    add firebase & pwa

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,12 +8,6 @@ var filesToCache = [
 // Cache on install
 self.addEventListener("install", event => {
     this.skipWaiting();
-    event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll(filesToCache);
-            })
-    )
 });
 
 // Clear cache on activate
