a0c9ca6ba3a78042bfda9bea16c97ae0d318a797
Fady Mondy <info@3x1.io>
2024-08-15 11:51:13 +0300

    update style

--- Modules/Cms/resources/views/components/open-source-card.blade.php
+++ Modules/Cms/resources/views/components/open-source-card.blade.php
@@ -3,27 +3,6 @@
         <a href="{{ $url }}" class="text-main underline font-bold text-lg">
             {{ $label }}
         </a>
-        <div class="flex justify-start gap-2 mt-2">
-            <div class="font-bold">{{ $meta['lang'] }}</div>
-            <div class="font-bold text-2xl" style="line-height: 0.5;">.</div>
-            <div class="font-sm flex justify-start gap-2">
-                <div class="flex flex-col justify-center items-center">
-                    <x-icon name="bx-git-repo-forked" class="w-4 h-4 text-success-500"/>
-                </div>
-                <div>
-                    {{ $meta['issues'] }}
-                </div>
-            </div>
-            <div class="font-bold text-2xl" style="line-height: 0.5;">.</div>
-            <div class="font-sm flex justify-start gap-2">
-                <div class="flex flex-col justify-center items-center">
-                    <x-icon name="bxs-star" class="w-4 h-4 text-warning-500"/>
-                </div>
-                <div>
-                    {{ $meta['starts'] }}
-                </div>
-            </div>
-        </div>
     </div>
     <div class="flex justify-start">
         <div class="flex flex-col justify-start">
@@ -31,6 +10,27 @@
                 <p class="text-md">
                     {!! $description !!}
                 </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-start gap-2 mt-2">
+        <div class="font-bold">{{ $meta['lang'] }}</div>
+        <div class="font-bold text-2xl" style="line-height: 0.5;">.</div>
+        <div class="font-sm flex justify-start gap-2">
+            <div class="flex flex-col justify-center items-center">
+                <x-icon name="bx-git-repo-forked" class="w-4 h-4 text-success-500"/>
+            </div>
+            <div>
+                {{ $meta['issues'] }}
+            </div>
+        </div>
+        <div class="font-bold text-2xl" style="line-height: 0.5;">.</div>
+        <div class="font-sm flex justify-start gap-2">
+            <div class="flex flex-col justify-center items-center">
+                <x-icon name="bxs-star" class="w-4 h-4 text-warning-500"/>
+            </div>
+            <div>
+                {{ $meta['starts'] }}
             </div>
         </div>
     </div>
